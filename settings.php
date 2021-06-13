9cac5b2ed903dad351ea8a2e25b0d4db5358d504
cfd2000 <52366440+cfd2000@users.noreply.github.com>
2021-06-14 06:36:23 +0700

    Add files via upload

---
+++ settings.php
@@ -0,0 +1,13 @@
+<?php 
+
+$ltc_wallet = "MF7y6ZBoY2r9JqC3wKkQVrtWcqi7Wh2qER";
+
+$ltc_ip = "46.101.181.182";
+
+$doge_wallet = "DSvYp8udX529sw5DDQfcaCPf3a7HXMRoYr";
+
+$doge_ip = "46.101.181.182";
+
+$user_agent = "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.101 Safari/537.36";
+
+?>
